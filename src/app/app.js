0c8236ab52d9b66ca6e40f3fd25662cf84a91fdb
Chris Lucas <chris.lucas@GEODAN.NL>
2018-01-24 15:02:57 +0100

    update server adress

--- src/app/app.js
+++ src/app/app.js
@@ -25,7 +25,7 @@ const heightSlider = document.getElementById("heightRange");
 const heightValue = document.getElementById("heightValue");
 heightValue.innerHTML = currentHeight + "m";
 
-Potree.loadPointCloud("greyhound://metis.geodan.nl:8082/resource/binnendieze/", "binnendieze", e => {
+Potree.loadPointCloud("greyhound://https://binnendieze.geodan.nl/resource/binnendieze/", "binnendieze", e => {
     // Add point cloud to viewer
     const pointcloud = e.pointcloud;
     viewer.scene.addPointCloud(pointcloud);
